624f3e031cf8139a8de29b3d36f1105143b804d3
Harrison tran <harrisontran@outlook.com>
2018-04-25 00:54:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ function placeOrder(cardNumber) {
     return "Sorry, we don't have a credit card on file for you."
   } else{
   
-    var temp = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}. `
+    var temp = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     getCart().splice(0);
     
     return temp
