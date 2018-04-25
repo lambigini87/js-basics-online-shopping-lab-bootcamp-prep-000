c6c40078edd49fcf7f7e50692b112cbd6df005d7
Harrison tran <harrisontran@outlook.com>
2018-04-25 00:43:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   
-  if( cardNumber !== isNaN() ){
+  if( cardNumber === null ){
     return "Sorry, we don't have a credit card on file for you."
   } else{
   
