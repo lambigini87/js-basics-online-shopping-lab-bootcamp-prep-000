5327055c2d6f5ec87f282923f3391edf9e07a2f0
Harrison tran <harrisontran@outlook.com>
2018-04-25 00:30:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ function placeOrder(cardNumber) {
   if( cardNumber === null){
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    getCart() = []
+    getCart() = [];
     
     return `Your total cost is $${total()},  which will be charged to the card ${cardNumber}. `
   }
